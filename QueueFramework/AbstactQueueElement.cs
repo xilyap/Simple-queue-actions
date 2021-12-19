472e87446c63e4201de87bdca27bb729361ca053
Ilya Podryadov <70620791+xilyap@users.noreply.github.com>
2021-12-19 18:31:25 +0500

    Delete QueueFramework directory

--- QueueFramework/AbstactQueueElement.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public abstract class AbstactQueueElement : MonoBehaviour
-{
-    [SerializeField]
-    private bool isActive;
-
-    public virtual bool IsActive { get => isActive; set => isActive = value; }
-}
